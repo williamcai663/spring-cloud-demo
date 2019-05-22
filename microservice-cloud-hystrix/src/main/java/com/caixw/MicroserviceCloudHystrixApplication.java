836669cf19847ddd899eba4bee302e3a93923ea8
caixiwen <williamcai663@gmail.com>
2019-05-22 20:13:38 +0800

    加入网管

--- microservice-cloud-hystrix/src/main/java/com/caixw/MicroserviceCloudHystrixApplication.java
+++ microservice-cloud-hystrix/src/main/java/com/caixw/MicroserviceCloudHystrixApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableHystrixDashboard
+//@EnableHystrix
+//@EnableEurekaClient
 public class MicroserviceCloudHystrixApplication {
 
 	public static void main(String[] args) {
--- microservice-eureka-user/src/main/java/com/caixw/controller/UserController.java
+++ microservice-eureka-user/src/main/java/com/caixw/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.caixw.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +14,21 @@ public class UserController {
     private RestTemplate restTemplate;
     @Value("${target.service.url}")
     private String serviceUrl;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
     }
 
     @GetMapping("/order")
-    public  String getOrder(){
+    @HystrixCommand(fallbackMethod = "callBack")
+    public String getOrder() {
         System.out.println(serviceUrl);
-        String result=restTemplate.getForObject(serviceUrl+"/order",String.class);
+        String result = restTemplate.getForObject(serviceUrl + "/order", String.class);
         return result;
+    }
+
+    private String callBack() {
+        return "调用失败,请重试";
     }
 }
